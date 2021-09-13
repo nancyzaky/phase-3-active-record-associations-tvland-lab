9e74d91e061e1fdc1b93df8b334d716256a9bb2d
Nancy Zaky <nancyzki@yahoo.com>
2021-09-13 13:26:57 -0700

    commit tree 1e3a9b3a0e36fcb3aaac548aa690b2ed37a66022

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+
+  def actors_list
+    self.actors.map do |item|
+      item['name']
+    end
+  end
 end
